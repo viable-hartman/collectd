e47598b6186a6193d76abfa37698ac60749c43dd
Jeremy Katz <jekatz@google.com>
2014-12-01 22:57:43 -0500

    Build the agent from the zookeeper branch right now

--- stackdriver/package-build.sh
+++ stackdriver/package-build.sh
@@ -22,6 +22,7 @@ then
     [ -d agent-rpm ] || git clone git@github.com:Stackdriver/agent-rpm.git
     pushd agent-rpm
     git pull
+    git checkout origin/zookeeper
     make clean
     make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
     if [ $? -ne 0 ]
