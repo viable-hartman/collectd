6f168ec9b1e266d4dd3bca6fde79f9b915843602
Corey Kosak <kosak@google.com>
2015-03-03 15:57:42 -0500

    stackdriver install and readme 1. Let's explicitly checkout a branch name rather than relying on the repo default. 2. Let's fail out if the agent-deb or agent-rpm directories already exist.

---
+++ stackdriver/package-build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -xe
+VERSION=5.5.0
+
+if [ "x$PKGFORMAT" == "xdeb" ]
+then
+    # debian denotes 64 arches with amd64
+    [ "x$ARCH" == "xx86_64" ] && ARCH="amd64" || true
+    git clone git@github.com:Stackdriver/agent-deb.git
+    git checkout stackdriver-agent-$VERSION
+    pushd agent-deb
+    make clean
+    make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
+    if [ $? -ne 0 ]
+    then
+        exit $?
+    fi
+	popd
+	[ -d result ] && rm -rf result || true
+	cp -r agent-deb/result .
+elif [ "x$PKGFORMAT" == "xrpm" ]
+then
+    git clone git@github.com:Stackdriver/agent-rpm.git
+    git checkout stackdriver-agent-$VERSION
+    pushd agent-rpm
+    make clean
+    make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
+    if [ $? -ne 0 ]
+    then
+        exit $?
+    fi
+    popd
+    [ -d result ] && rm -rf result || true
+	cp -r agent-rpm/result .
+else
+    echo "I don't know how to handle label '$PKGFORMAT'. Aborting build"
+    exit 1
+fi
+
