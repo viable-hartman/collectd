f62108e807054e12500e5a1592a9c1fa384c0f58
Corey Kosak <kosak@google.com>
2015-06-17 16:27:09 -0400

    Bump the buffer size for the write_http plugin and force flushing semi-frequently.  This isn't a long-term answer but makes it so that we receive all of the metrics for a time period at once so that we can turn them into a percent when we receive them rather than having to do that downstream.
    Longer term, we should try to get the ability for the plugins
    we care about % values of to send that % directly and then
    we can drop this patch.

--- src/write_http.c
+++ src/write_http.c
@@ -33,11 +33,15 @@
 #include <curl/curl.h>
 
 #ifndef WRITE_HTTP_DEFAULT_BUFFER_SIZE
+<<<<<<< HEAD
 #define WRITE_HTTP_DEFAULT_BUFFER_SIZE 4096
 #endif
 
 #ifndef WRITE_HTTP_DEFAULT_PREFIX
 #define WRITE_HTTP_DEFAULT_PREFIX "collectd"
+=======
+# define WRITE_HTTP_DEFAULT_BUFFER_SIZE (1024 * 1024)
+>>>>>>> Bump the buffer size for the write_http plugin and force flushing
 #endif
 
 /*
@@ -81,10 +85,22 @@ struct wh_callback_s {
   size_t send_buffer_fill;
   cdtime_t send_buffer_init_time;
 
+<<<<<<< HEAD
   pthread_mutex_t send_lock;
 
   int data_ttl;
   char *metrics_prefix;
+=======
+        char  *send_buffer;
+        size_t send_buffer_size;
+        size_t send_buffer_free;
+        size_t send_buffer_fill;
+        cdtime_t send_buffer_init_time;
+        cdtime_t buffer_flush_last;
+
+        pthread_mutex_t send_lock;
+        pthread_mutex_t flush_lock;
+>>>>>>> Bump the buffer size for the write_http plugin and force flushing
 };
 typedef struct wh_callback_s wh_callback_t;
 
@@ -512,6 +528,7 @@ static int wh_write_kairosdb(const data_set_t *ds,
   return 0;
 } /* }}} int wh_write_kairosdb */
 
+<<<<<<< HEAD
 static int wh_write(const data_set_t *ds, const value_list_t *vl, /* {{{ */
                     user_data_t *user_data) {
   wh_callback_t *cb;
@@ -535,6 +552,44 @@ static int wh_write(const data_set_t *ds, const value_list_t *vl, /* {{{ */
     break;
   }
   return status;
+=======
+static int wh_write (const data_set_t *ds, const value_list_t *vl, /* {{{ */
+                user_data_t *user_data)
+{
+        wh_callback_t *cb;
+        int status;
+        cdtime_t now;
+
+        if (user_data == NULL)
+                return (-EINVAL);
+
+        cb = user_data->data;
+        assert (cb->send_metrics);
+
+        /* we want a large buffer so we get all the measurements for a time at once
+        but we also want to force flushing it every minute. this will do. longer term,
+        this could be a configurable part of a stackdriver specific write plugin */
+        pthread_mutex_lock (&cb->flush_lock);
+        now = cdtime ();
+        if (now > cb->send_buffer_init_time + MS_TO_CDTIME_T(15000)) {
+            wh_flush(0, NULL, user_data);
+            cb->buffer_flush_last = now;
+        }
+        pthread_mutex_unlock (&cb->flush_lock);
+
+        switch(cb->format) {
+            case WH_FORMAT_JSON:
+                status = wh_write_json (ds, vl, cb);
+                break;
+            case WH_FORMAT_KAIROSDB:
+                status = wh_write_kairosdb (ds, vl, cb);
+                break;
+            default:
+                status = wh_write_command (ds, vl, cb);
+                break;
+        }
+        return (status);
+>>>>>>> Bump the buffer size for the write_http plugin and force flushing
 } /* }}} int wh_write */
 
 static int wh_notify(notification_t const *n, user_data_t *ud) /* {{{ */
