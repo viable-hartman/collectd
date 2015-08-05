213b8aef89fe6a5219bff839159dd021df03218d
Corey Kosak <kosak@google.com>
2015-08-05 16:59:31 -0400

    Ensure that mysql_thread_init() is called from every thread, and that the plugin calls mysql_library_init at the beginning. (bring over patch from the collectd-5.4.2 branch)

--- src/mysql.c
+++ src/mysql.c
@@ -258,6 +258,7 @@ static int mysql_config(oconfig_item_t *ci) /* {{{ */
 static MYSQL *getconnection(mysql_database_t *db) {
   const char *cipher;
 
+  mysql_thread_init();
   if (db->is_connected) {
     int status;
 
@@ -942,5 +943,6 @@ static int mysql_read(user_data_t *ud) {
 } /* int mysql_read */
 
 void module_register(void) {
+  mysql_library_init(0, NULL, NULL);
   plugin_register_complex_config("mysql", mysql_config);
 } /* void module_register */
