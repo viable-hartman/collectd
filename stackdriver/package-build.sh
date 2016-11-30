02a900b39245445196574ea0d93d6ef1f47ba143
Dhrupad Bhardwaj <dhrupad@google.com>
2016-11-30 11:14:16 -0500

    Changed exit behaviour to return correct exit value. Formatting fix

--- stackdriver/package-build.sh
+++ stackdriver/package-build.sh
@@ -48,28 +48,24 @@ then
 >>>>>>> Change package-build.sh so that it checks out a branch specified by an
     pushd agent-deb
     make clean
-    make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
-    if [ $? -ne 0 ]
-    then
-        exit $?
-    fi
-	popd
-	[ -d result ] && rm -rf result || true
-	cp -r agent-deb/result .
+    make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build || exit $?
+    popd
+    [ -d result ] && rm -rf result || true
+    cp -r agent-deb/result .
 elif [ "x$PKGFORMAT" == "xrpm" ]
 then
     git clone git@github.com:Stackdriver/agent-rpm.git --branch $BRANCH
     pushd agent-rpm
     make clean
-    make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
-    if [ $? -ne 0 ]
-    then
-        exit $?
-    fi
+    make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build || exit $?
     popd
     [ -d result ] && rm -rf result || true
+<<<<<<< HEAD
 	cp -r agent-rpm/result .
 >>>>>>> stackdriver install and readme
+=======
+    cp -r agent-rpm/result .
+>>>>>>> Changed exit behaviour to return correct exit value. Formatting fix
 else
     echo "I don't know how to handle label '$PKGFORMAT'. Aborting build"
     exit 1
