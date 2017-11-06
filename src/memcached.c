1c5d95f103d64c952ab1ee83cd1a21b2e7f732e9
Florian Forster <octo@collectd.org>
2017-11-06 20:54:05 +0100

    Reformat changed files.

--- src/memcached.c
+++ src/memcached.c
@@ -683,7 +683,8 @@ static int memcached_add_read_callback(memcached_t *st) {
       /* group = */ "memcached",
       /* name      = */ callback_name,
       /* callback  = */ memcached_read,
-      /* interval  = */ 0, &(user_data_t){
+      /* interval  = */ 0,
+      &(user_data_t){
           .data = st, .free_func = memcached_free,
       });
 } /* int memcached_add_read_callback */
