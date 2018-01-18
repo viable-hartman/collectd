60d1fd985059dc4caf3f877aea95389a642cb520
campbellsean@google.com <campbellsean@google.com>
2018-01-18 16:17:06 -0500

    Add and enable WMI plugin.

--- build.sh
+++ build.sh
@@ -150,7 +150,8 @@ build_windows ()
 	  --datarootdir="${DATADIR}" \
 	  --disable-all-plugins \
 	  --host="mingw32" \
-	  --enable-logfile
+	  --enable-logfile \
+	  --enable-wmi
 
 	cp ${GNULIB_DIR}/../config.h src/gnulib_config.h
 	echo "#include <config.h.in>" >> src/gnulib_config.h
