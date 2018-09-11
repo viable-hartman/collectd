c9ac5e2414a810b96b01a25deed960cbaec1ae1c
Gabe Perez <perez283@gmail.com>
2018-09-11 15:17:30 -0400

    Travis build - distcheck missing man pages target, missing header in dist tarball, and clang errors (#135)
    * Fix issue with dist-hook missing man pages target.
    * Fix issue with missing header file from distribution tarball. This resolves the error seen during distcheck.
    * Fix clang compilation errors.
    * Remove sleep calls, similar to upstream collectd.

--- src/daemon/plugin.c
+++ src/daemon/plugin.c
@@ -694,7 +694,6 @@ static void stop_read_threads (void)
 	pthread_cond_broadcast (&read_cond);
 	pthread_mutex_unlock (&read_lock);
 
-	sleep(0.25);
 	for (int i = 0; i < read_threads_num; i++)
 	{
 		read_threads[i] = (pthread_t) 0;
@@ -911,7 +910,6 @@ static void stop_write_threads (void) /* {{{ */
 	pthread_cond_broadcast (&write_cond);
 	pthread_mutex_unlock (&write_lock);
 
-	sleep(0.25);
 	for (i = 0; i < write_threads_num; i++)
 	{
 		write_threads[i] = (pthread_t) 0;
