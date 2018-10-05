da1fafa7315d7cb5822062e5f91c651c2aa65d09
smallem <sridhar.mallem@oracle.com>
2018-10-04 23:15:04 -0400

    fixed format issues

--- src/exec.c
+++ src/exec.c
@@ -836,8 +836,7 @@ static void *exec_notification_one(void *arg) /* {{{ */
   else if (n->severity == NOTIF_OKAY)
     severity = "OKAY";
 
-  fprintf(fh,
-          "Severity: %s\n"
+  fprintf(fh, "Severity: %s\n"
           "Time: %.3f\n",
           severity, CDTIME_T_TO_DOUBLE(n->time));
 
