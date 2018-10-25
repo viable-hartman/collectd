75839bedb3cddf3013c061d220c75ee4a3c7537a
Nina-Marie Amadeo <namadeo@google.com>
2018-10-25 17:09:40 -0400

    Delete duplicate declaration of some_detail_active_g.

--- src/processes.c
+++ src/processes.c
@@ -447,8 +447,12 @@ static bool report_delay = 0;
 >>>>>>> Removes HEAD tag (atom bug) from remaining files... I think.
 =======
 static want_detail_configuration_t want_detail_configuration_g;
+<<<<<<< HEAD
 static _Bool some_detail_active_g = 0;	static _Bool some_detail_active_g = 0;
 >>>>>>> Adds back in declaration of want_detail_configuration_g.
+=======
+static _Bool some_detail_active_g = 0;
+>>>>>>> Delete duplicate declaration of some_detail_active_g.
 static bool want_init = true;
 >>>>>>> Set static bools to true instead of 1
 static bool report_ctx_switch;
