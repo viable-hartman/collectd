67eb2c839db5a9ac302c6310ed23fd89dc96c104
Nina-Marie Amadeo <namadeo@google.com>
2018-11-09 14:41:14 -0500

    Changes ps to pse as argument to ps_read_process calls

--- src/processes.c
+++ src/processes.c
@@ -2623,7 +2623,7 @@ static int ps_read(void) {
 		pse.gauges = ps.gauges;
 		pse.counters = ps.counters;
 
-    status = ps_read_process(pid, &pse, &state);
+    status = ps_read_process(pid, &ps, &state);
     if (status != 0) {
       DEBUG("ps_read_process failed: %i", status);
       continue;
@@ -3223,7 +3223,7 @@ static int ps_read(void) {
 		pse.gauges = ps.gauges;
 		pse.counters = ps.counters;
 
-    status = ps_read_process(pid, &pse, &state);
+    status = ps_read_process(pid, &ps, &state);
     if (status != 0) {
       DEBUG("ps_read_process failed: %i", status);
       continue;
