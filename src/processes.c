54ac6fe07b5712dca765efdc34b72e9a082b5155
Nina-Marie Amadeo <namadeo@google.com>
2018-11-13 14:38:14 -0500

    num_maps

--- src/processes.c
+++ src/processes.c
@@ -181,6 +181,7 @@
 typedef struct procstat_gauges_s {
 	unsigned long num_proc;
 	unsigned long num_lwp;
+  unsigned long num_maps;
 	unsigned long vmem_size;
 	unsigned long vmem_rss;
 	unsigned long vmem_data;
@@ -230,6 +231,7 @@ typedef struct process_entry_s {
   char name[PROCSTAT_NAME_LEN];
 
   unsigned long num_proc;
+  unsigned long num_maps;
   unsigned long num_lwp;
   unsigned long num_fd;
   unsigned long num_maps;
