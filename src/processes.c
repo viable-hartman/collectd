43dad193bd2d3f8617c10733d83b66bbc5b7b3ce
Nina-Marie Amadeo <namadeo@google.com>
2018-11-20 10:53:10 -0500

    Replaces process_entry_t with procstat_entry_t

--- src/processes.c
+++ src/processes.c
@@ -348,7 +348,7 @@ typedef struct process_entry_s {
   bool has_fd;
 
   bool has_maps;
-} process_entry_t;
+} procstat_entry_t;
 
 
 
@@ -568,7 +568,7 @@ static mach_msg_type_number_t pset_list_len;
 
 #elif KERNEL_LINUX
 static long pagesize_g;
-static void ps_fill_details(const procstat_t *ps, process_entry_t *entry);
+static void ps_fill_details(const procstat_t *ps, procstat_entry_t *entry);
 /* #endif KERNEL_LINUX */
 
 #elif HAVE_LIBKVM_GETPROCS &&                                                  \
@@ -822,7 +822,7 @@ static void ps_update_counter (derive_t *group_counter, derive_t *curr_counter,
 }
 
 static void ps_update_delay(procstat_t *out, procstat_entry_t *prev,
-                            process_entry_t *curr) {
+                            procstat_entry_t *curr) {
   cdtime_t now = cdtime();
 
   ps_update_delay_one(&out->delay_cpu, &prev->delay_cpu, curr->delay.cpu_ns,
@@ -1790,6 +1790,31 @@ static void dispatch_value_helper (value_list_t *vl,
     vl->values_len = values_len;
     plugin_dispatch_values(vl);
 }
+<<<<<<< HEAD
+=======
+#endif /* KERNEL_LINUX || KERNEL_SOLARIS*/
+
+/* ------- additional functions for KERNEL_LINUX/HAVE_THREAD_INFO ------- */
+#if KERNEL_LINUX
+static int ps_read_tasks_status(procstat_entry_t *ps) {
+  char dirname[64];
+  DIR *dh;
+  char filename[64];
+  FILE *fh;
+  struct dirent *ent;
+  derive_t cswitch_vol = 0;
+  derive_t cswitch_invol = 0;
+  char buffer[1024];
+  char *fields[8];
+  int numfields;
+
+  snprintf(dirname, sizeof(dirname), "/proc/%li/task", ps->id);
+
+  if ((dh = opendir(dirname)) == NULL) {
+    DEBUG("Failed to open directory `%s'", dirname);
+    return -1;
+  }
+>>>>>>> Replaces process_entry_t with procstat_entry_t
 
 static void ps_submit_proc_stats (
         _Bool doing_detail,
@@ -2106,7 +2131,11 @@ static procstat_gauges_t *ps_read_tasks_status (long pid, procstat_gauges_t *g)
 
 /* Read data from /proc/pid/status */
 <<<<<<< HEAD
+<<<<<<< HEAD
 static int ps_read_status(long pid, process_entry_t *ps) {
+=======
+static int ps_read_status(long pid, procstat_entry_t *ps) {
+>>>>>>> Replaces process_entry_t with procstat_entry_t
   FILE *fh;
   char buffer[1024];
   char filename[64];
@@ -2161,7 +2190,7 @@ static int ps_read_status(long pid, process_entry_t *ps) {
   return 0;
 } /* int *ps_read_status */
 
-static int ps_read_io(process_entry_t *ps) {
+static int ps_read_io(procstat_entry_t *ps) {
   FILE *fh;
   char buffer[1024];
   char filename[64];
@@ -2263,7 +2292,7 @@ static int ps_count_fd(int pid) {
 } /* int ps_count_fd (pid) */
 
 #if HAVE_LIBTASKSTATS
-static int ps_delay(process_entry_t *ps) {
+static int ps_delay(procstat_entry_t *ps) {
   if (taskstats_handle == NULL) {
     return ENOTCONN;
   }
@@ -2580,7 +2609,11 @@ static int ps_read_process (long pid, procstat_t *ps, char *state)
 #endif
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 static void ps_fill_details(const procstat_t *ps, process_entry_t *entry) {
+=======
+static void ps_fill_details(const procstat_t *ps, procstat_entry_t *entry) {
+>>>>>>> Replaces process_entry_t with procstat_entry_t
   if (entry->has_io == false) {
     ps_read_io(entry);
     entry->has_io = true;
@@ -3396,9 +3429,13 @@ static char *ps_get_cmdline(long pid,
  */
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> Removes HEAD tag (atom bug) from remaining files... I think.
 static int ps_read_process(long pid, process_entry_t *ps, char *state) {
+=======
+static int ps_read_process(long pid, procstat_entry_t *ps, char *state) {
+>>>>>>> Replaces process_entry_t with procstat_entry_t
   char filename[64];
   char f_psinfo[64], f_usage[64];
   char *buffer;
@@ -3713,7 +3750,7 @@ static int ps_read(void) {
   int blocked = 0;
 
   procstat_t *ps;
-  process_entry_t pse;
+  procstat_entry_t pse;
 
   ps_list_reset();
 
@@ -4300,7 +4337,7 @@ static int ps_read(void) {
   struct kinfo_proc *proc_ptr = NULL;
   int count; /* returns number of processes */
 
-  process_entry_t pse;
+  procstat_entry_t pse;
 
   ps_list_reset();
 
@@ -4460,7 +4497,7 @@ static int ps_read(void) {
   struct kinfo_proc *proc_ptr = NULL;
   int count; /* returns number of processes */
 
-  process_entry_t pse;
+  procstat_entry_t pse;
 
   ps_list_reset();
 
@@ -4895,7 +4932,7 @@ static int ps_read(void) {
   pid_t pindex = 0;
   int nprocs;
 
-  process_entry_t pse;
+  procstat_entry_t pse;
 
   ps_list_reset();
   while ((nprocs = getprocs64(procentry, sizeof(struct procentry64),
@@ -5196,7 +5233,7 @@ static int ps_read(void) {
   while ((ent = readdir(proc)) != NULL) {
     long pid;
     struct procstat ps;
-    process_entry_t pse;
+    procstat_entry_t pse;
     char *endptr;
 
     if (!isdigit((int)ent->d_name[0]))
