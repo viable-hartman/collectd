4753053cc4466e75aea025a7b58d62d48c3ec2ad
Nina-Marie Amadeo <namadeo@google.com>
2018-11-29 12:37:08 -0500

    message change for testing

--- src/daemon/filter_chain.c
+++ src/daemon/filter_chain.c
@@ -689,7 +689,7 @@ static int fc_bit_write_invoke(const data_set_t *ds, /* {{{ */
       {
         c_complain (LOG_INFO, &plugin_list[i].complaint,
             "Filter subsystem: Built-in target `write': Dispatching value to "
-            "the `%s' plugin failed with status %i.",
+            "the `%s' plugin TEST failed with status %i.",
             plugin_list[i].plugin, status);
 
         plugin_log_available_writers();
