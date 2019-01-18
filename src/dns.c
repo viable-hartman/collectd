37682b567d52ea53eb4789f1c2cb84d248b6a2f0
Nina-Marie Amadeo <namadeo@google.com>
2019-01-18 16:55:35 -0500

    Fixes dns.c

--- src/dns.c
+++ src/dns.c
@@ -397,4 +397,4 @@ void module_register(void) {
   plugin_register_config("dns", dns_config, config_keys, config_keys_num);
   plugin_register_init("dns", dns_init);
   plugin_register_read("dns", dns_read);
-} /* void module_register */
+}
